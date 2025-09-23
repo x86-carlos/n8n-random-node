f12f459d6fe8e94215b609f6cf61ccfb933a924f
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2025-09-22 21:23:40 -0300

    feat: define the new nodo to generate random numbers

---
+++ n8n-random-node/nodes/Random/Random.node.ts
@@ -0,0 +1,131 @@
+import {
+	IExecuteFunctions,
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+export class Random implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Random',
+		name: 'random',
+		icon: 'file:integral.svg',
+		group: ['transform'],
+		description: 'Conector que retorna um número aleatório num intervalo definido pelo usuário',
+		defaults: {
+			name: 'random',
+		}
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'randomNodeCredential',
+				required: 'true',
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name: 'Gerar',
+						value: 'gerar',
+					},
+				],
+				noDataExpression: true,
+				descriptions: 'Gera um número aleatório',
+			},
+
+			{
+				displayName: 'Options',
+				name: 'option',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: [
+							'gerar',
+						],
+					},
+				},
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Gerar',
+						value: 'gerar',
+						action: 'Gerar um número aleatório'
+						description: 'Gerar um número aleatório no intervalo especificado'
+					},
+				],
+			},
+
+			{
+				displayName: 'Min',
+				name: 'minNumber',
+				required: true,
+				type: 'number',
+				typeOptions: {
+					numberPrecision:0,
+				},
+				displayOptions: {
+					show: {
+						resource: [
+							'gerar',	
+						],
+						operation: [
+							'gerar',
+						],
+					},
+				},
+				default: 1,
+			},
+
+			{
+				displayName: 'Max',
+				name: 'maxNumber',
+				required: true,
+				type: 'number',
+				typeOptions: {
+					numberPrecision:0,
+				},
+				displayOptions: {
+					show: {
+						resource: [
+							'gerar',	
+						],
+						operation: [
+							'gerar',
+						],
+					},
+				},
+				default: 1,
+			},
+		], //End Properties
+	}; //End Description
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const resource = this.getNodeParameter('resource', 0) as string;
+		const operation = this.getNodeParameter('option', 0) as string;
+		let responseData;
+
+		if(resource === 'gerar' && operation === 'gerar') {
+			const min = this.getNodeParameter('minNumber', 0) as string;
+			const max = this.getNodeParameter('maxNumber', 0) as string;
+			const options = {
+				headers: {
+					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+				},
+				uri: `https://www.random.org/integers/?num=1&min=${min}&max=${max}&col=1&base=10&format=plain&rnd=new`,
+			};
+			
+			try {
+				responseData = await this.helpers.requestWithAuthentication.call(this,'randomNodeCredential', options);
+			} catch(error) {
+				throw new Error(`Random Number Generato API error: ${error.message}`);
+			}
+		}
+	}
+
+} //End Class
